2a7d4d887db49bab7963af48ff55f87a69ea47f1
Dominik Trusinski <dominik.trusinski@gmail.com>
2018-08-26 18:47:20 +0200

    Delete unnecessary IO.puts. Delete unnecessary tests

--- test/hadrian/registration/registration_test.exs
+++ test/hadrian/registration/registration_test.exs
@@ -10,17 +10,9 @@ defmodule Hadrian.RegistrationTest do
       assert :error = Registration.register_user(%{}, :invalid)
     end
 
-    test "returns {:ok, user} tuple when registering using in app option" do
-      user_attrs = build(:user_attrs)
-
-      assert {:ok, %User{} = user} = Registration.register_user(user_attrs, :in_app)
-    end
-
     test "returns {:ok, user} tuple when registering using facebook option" do
       user_attrs = build(:user_attrs)
       |> Map.update("password", nil, fn val -> nil end)
-
-      user_attrs |> inspect |> IO.puts
 
       assert {:ok, %User{} = user} = Registration.register_user(user_attrs, :facebook)
     end
