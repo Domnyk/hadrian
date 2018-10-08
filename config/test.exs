72c50b92062cfa78f9dba218bbb9f0d75568c445
Unknown <dominik.trusinski@gmail.com>
2018-10-08 21:41:28 +0200

    Print only errors during test

--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,8 @@ config :hadrian, HadrianWeb.Endpoint,
   http: [port: 4001],
   server: false
 
-# Print only warnings and errors during test
-config :logger, level: :warn
+# Print only errors during test
+config :logger, level: :error
 
 # Configure your database
 config :hadrian, Hadrian.Repo,
