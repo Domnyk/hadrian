a8a17732f864150aa42633eb7a7ea63aa5897e97
Unknown <dominik.trusinski@gmail.com>
2018-11-02 20:04:26 +0100

    Fix tests

--- test/hadrian_web/api/controllers/sport_arena_controller_test.exs
+++ test/hadrian_web/api/controllers/sport_arena_controller_test.exs
@@ -15,27 +15,29 @@ defmodule HadrianWeb.Api.SportArenaControllerTest do
     basketball = insert(:sport_discipline, name: "Basketball")
     volleyball = insert(:sport_discipline, name: "Volleyball")
     sport_disciplines = [football, basketball, volleyball]
-    sport_disciplines_as_maps = Enum.map(sport_disciplines, &(%{"id" => &1.id, "name" => &1.name}))
+    sport_disciplines_ids = Enum.map(sport_disciplines, & &1.id)
+    sport_disciplines_as_map = Enum.map(sport_disciplines, &%{"id" => &1.id, "name" => &1.name})
 
     {
       :ok, conn: put_req_header(conn, "accept", "application/json"), sport_complex: sport_complex,
       sport_object: sport_object,
       sport_arena: sport_arena,
       sport_disciplines: sport_disciplines,
-      sport_disciplines_as_maps: sport_disciplines_as_maps
+      sport_disciplines_ids: sport_disciplines_ids,
+      sport_disciplines_as_map: sport_disciplines_as_map
     }
   end
 
   describe "index" do
     test "lists all sport_arenas in sport complex", %{conn: conn, sport_arena: %SportArena{id: id, name: name},
                                                       sport_object: sport_object, sport_disciplines: sport_disciplines,
-                                                      sport_disciplines_as_maps: sport_disciplines_as_maps,
+                                                      sport_disciplines_as_map: sport_disciplines_as_map,
                                                       sport_complex: _} do
       insert_sport_disciplines_in_sport_arena(id, sport_disciplines)
       conn = get conn, sport_object_sport_arena_path(conn, :index, sport_object.id)
       resp = json_response(conn, 200)
       assert resp["status"] == "ok"
-      assert [%{"id" => ^id, "name" => ^name, "sport_disciplines" => ^sport_disciplines_as_maps}] = resp["data"]["sport_arenas"]
+      assert [%{"id" => ^id, "name" => ^name, "sport_disciplines" => ^sport_disciplines_as_map}] = resp["data"]["sport_arenas"]
     end
 
     defp insert_sport_disciplines_in_sport_arena(sport_arena_id, sport_disciplines) do
@@ -50,17 +52,18 @@ defmodule HadrianWeb.Api.SportArenaControllerTest do
 
   describe "create sport_arena" do
     test "renders sport arena when data is valid", %{conn: conn, sport_complex: _, sport_object: sport_object,
-                                                     sport_disciplines_as_maps: sport_disciplines_as_maps} do
+                                                     sport_disciplines_ids: sport_disciplines_ids,
+                                                     sport_disciplines_as_map: sport_disciplines_as_map} do
       params = build(:sport_arena_params)
                |> Kernel.put_in(["data", "sport_arena", "sport_object_id"], sport_object.id)
-               |> Kernel.put_in(["data", "sport_arena", "sport_disciplines"], sport_disciplines_as_maps)
+               |> Kernel.put_in(["data", "sport_arena", "sport_disciplines"], sport_disciplines_ids)
 
       conn = post conn, sport_object_sport_arena_path(conn, :create, sport_object.id), params
       resp = json_response(conn, 201)["data"]["sport_arena"]
       sport_arena_from_db = Repo.get_by(SportArena, name: resp["name"])
 
       assert resp["name"] == sport_arena_from_db.name
-      assert resp["sport_disciplines"] == sport_disciplines_as_maps
+      assert resp["sport_disciplines"] == sport_disciplines_as_map
     end
 
     test "renders errors when data is invalid", %{conn: conn, sport_complex: _, sport_object: sport_object} do
@@ -77,16 +80,16 @@ defmodule HadrianWeb.Api.SportArenaControllerTest do
 
     test "renders sport arena when data is valid", %{conn: conn, sport_complex: _, sport_object: _,
                                                      sport_arena: sport_arena,
-                                                     sport_disciplines_as_maps: sport_disciplines_as_maps} do
-      [%{"id" => id, "name" => name} = updated_sport_discipline | _] = sport_disciplines_as_maps
+                                                     sport_disciplines_ids: sport_disciplines_ids} do
+      [updated_sport_discipline_id | _] = sport_disciplines_ids
       updated_name = "Some updated name"
-      conn = put conn, sport_arena_path(conn, :update, sport_arena.id), build_params(updated_sport_discipline, updated_name)
+      conn = put conn, sport_arena_path(conn, :update, sport_arena.id), build_params(updated_sport_discipline_id, updated_name)
       json_response(conn, 200)
 
       updated_sport_arena = Owners.get_sport_arena!(sport_arena.id) |> Repo.preload(:sport_disciplines)
       assert updated_sport_arena.id == sport_arena.id
       assert updated_sport_arena.name == updated_name
-      assert [%SportDiscipline{id: ^id, name: ^name}] = updated_sport_arena.sport_disciplines
+      assert [Hadrian.Repo.get(SportDiscipline, updated_sport_discipline_id)] == updated_sport_arena.sport_disciplines
     end
 
     test "renders errors when data is invalid", %{conn: conn, sport_complex: _, sport_object: _,
@@ -97,10 +100,10 @@ defmodule HadrianWeb.Api.SportArenaControllerTest do
       assert resp["errors"] != %{}
     end
 
-    defp build_params(sport_disciplines, name) do
+    defp build_params(sport_disciplines_ids, name) do
       build(:sport_arena_params)
       |> Kernel.put_in(["data", "sport_arena", "name"], name)
-      |> Kernel.put_in(["data", "sport_arena", "sport_disciplines"], [sport_disciplines])
+      |> Kernel.put_in(["data", "sport_arena", "sport_disciplines"], [sport_disciplines_ids])
     end
   end
   
