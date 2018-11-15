237cb857601a9a16f66114257137d4ee84cb8029
Unknown <dominik.trusinski@gmail.com>
2018-11-15 23:26:29 +0100

    Changes in sport object migration file

--- priv/repo/migrations/20180711185102_add_sport_objects.exs
+++ priv/repo/migrations/20180711185102_add_sport_objects.exs
@@ -4,8 +4,6 @@ defmodule Hadrian.Repo.Migrations.AddSportObjects do
   def up do
     create table(:sport_objects) do
       add :name,              :varchar, size: 100
-      add :longitude,         :decimal, precision: 9, scale: 6
-      add :latitude,          :decimal, precision: 8, scale: 6
       add :geo_coordinates,   :map
       add :address,           :map
       add :booking_margin,    :interval
