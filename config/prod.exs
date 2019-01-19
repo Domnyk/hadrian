fbe91cf9ea15c1e0855c56a2ba335a9b55f5a165
Dominik Trusinski <dominik.trusinski@gmail.com>
2019-01-19 20:24:56 +0100

    Cookies suck

--- config/prod.exs
+++ config/prod.exs
@@ -76,8 +76,6 @@ config :hadrian, :access_token, Hadrian.Session.Facebook.AccessToken.HTTP
 # Configure client url
 config :hadrian, :client_url, "https://vinci-11235813.herokuapp.com"
 
-config :hadrian, :domain, ".vinci-11235813.herokuapp.com"
-
 # Paypal
 config :hadrian, :api_url, "https://api.sandbox.paypal.com"
 config :hadrian, :paypal_storage, Hadrian.PaypalStorage
