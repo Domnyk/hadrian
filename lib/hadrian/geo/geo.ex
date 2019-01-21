c32df1c16f2a63b30058347af4a9af52e2a642ea
Dominik Trusinski <dominik.trusinski@gmail.com>
2019-01-21 19:01:58 +0100

    Small changes

--- lib/hadrian/geo/geo.ex
+++ lib/hadrian/geo/geo.ex
@@ -1,28 +1,24 @@
 defmodule Hadrian.Geo do
-  @earth_radius_km 6371
-  @d2r :math.pi / 180
+  @earth_rad_in_km 6371
+  @deg_to_rad_factor :math.pi / 180
 
-  def deg_to_rad(deg), do: deg * @d2r
+  def deg_to_rad(deg), do: deg * @deg_to_rad_factor
 
-  def distance(p1, p2, :km), do: haversine(p1, p2) * @earth_radius_km
+  def distance(p1, p2, :km), do: haversine(p1, p2) * @earth_rad_in_km
   def distance(p1, p2, :m), do: distance(p1, p2, :km) * 1000
 
   @doc """
-  Calculate the [Haversine](https://en.wikipedia.org/wiki/Haversine_formula)
-  distance between two coordinates. Result is in radians. This result can be
-  multiplied by the sphere's radius in any unit to get the distance in that unit.
-  For example, multiple the result of this function by the Earth's radius in
-  kilometres and you get the distance between the two given points in kilometres.
+  Calculate the Haversine distance between two coordinates. Result in radians.
   """
   def haversine({lat1, lon1}, {lat2, lon2}) do
-    dlat = deg_to_rad(lat2 - lat1)
-    dlon = deg_to_rad(lon2 - lon1)
+    d_longitude = deg_to_rad(lon2 - lon1)
+    d_latitude = deg_to_rad(lat2 - lat1)
 
     radlat1 = deg_to_rad(lat1)
     radlat2 = deg_to_rad(lat2)
 
-    a = :math.pow(:math.sin(dlat / 2), 2) +
-        :math.pow(:math.sin(dlon / 2), 2) *
+    a = :math.pow(:math.sin(d_latitude / 2), 2) +
+        :math.pow(:math.sin(d_longitude / 2), 2) *
         :math.cos(radlat1) * :math.cos(radlat2)
 
     2 * :math.atan2(:math.sqrt(a), :math.sqrt(1 - a))
